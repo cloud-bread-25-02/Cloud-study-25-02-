13cb30d1e1982e5b2bd24f73724c34534b084906
younggwang <kimyoungkwang9563@gmail.com>
2025-11-10 20:02:50 +0900

    project setup

---
+++ src/test/java/com/cloudIaas/cloudIaas/CloudIaasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudIaas.cloudIaas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudIaasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
