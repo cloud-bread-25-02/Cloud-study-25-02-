2e5e48e7884580bbd9cdd8b7636ae602b7280da7
MinJuTur <jmj4701jmj@gmail.com>
2025-11-17 21:48:45 +0900

    feat: 컨테이너 생성·조회·시작·중지·삭제 기능 초기 구현

---
+++ src/main/java/com/cloudIaas/cloudIaas/domain/UserInfo.java
@@ -0,0 +1,12 @@
+package com.cloudIaas.cloudIaas.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class UserInfo {
+    private String name;
+    private String userId;
+    private String email;
+}
